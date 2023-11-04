0393c22221c77521ac3ec34f4a89225ec9c04dce
kuldeepsingh-in <in.singhkuldeep@gmail.com>
2023-11-04 20:39:53 +0000

    New version of website updated from github

---
+++ Pages/kuldeep.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ResumeWebsite.Pages
+{
+    public class kuldeepModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
